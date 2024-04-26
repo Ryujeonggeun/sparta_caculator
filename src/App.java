4f6a7bfc140e56498602b34724e8af9043f72380
Ryujeonggeun <161789810+Ryujeonggeun@users.noreply.github.com>
2024-04-26 15:13:46 +0900

    1-5(배열 생성과 카운트)

--- src/App.java
+++ src/App.java
@@ -1,8 +1,13 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        //배열 생성
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        //count(결과값의 개수) 변수 생성
+        int count = 0;
 
         // 반복문 사용
         while (true) {
@@ -15,7 +20,7 @@ public class App {
         char sachic = sc.next().charAt(0);
 
 
-        double result = 0;
+        int result = 0;
 
         switch (sachic) {
             case '+':
@@ -36,11 +41,12 @@ public class App {
                     System.out.println("분모는 0이 될 수 없습니다.");
                     break;
                 }
-               result = (double) num1 / (double) num2;
+                 result = num1 / num2;
                 break;
 
             default:
                 System.out.println("알맞은 사칙연산이 아닙니다");
+                 break;//count가 올라가지 않도록 break 해준다
         }
 
         System.out.println("결과: " + result );
@@ -48,11 +54,17 @@ public class App {
             System.out.print("더 계산하시겠습니까? (exit 입력 시 종료)");
             String repeat = sc.next();
 
-            // exit 를 입력받으면 반복 종료
 
+            arr.add(result);
+            count++;
+            // exit 를 입력받으면 반복 종료
             if (repeat.equals("exit")) {
                 break;
             }
+
+            // 배열이 10개 가 넘으면 가장 먼저 저장된 값을 새로운 연산 결과로 바꿈
+            // 총 숫자 > 10 이면, set(count % 10 , result) 로 변겅, 이렇게하면 20이 넘어가도 값을 저장 가능
+
         }
         System.out.println("계산을 종료합니다");
     }
